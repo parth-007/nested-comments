3740fe2845c31f45dcc489a83f1d81179d74ffc3
Parth Mangukiya <parthmangukiya@gofynd.com>
2023-07-30 21:05:10 +0530

    GET Posts route

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-console.log("Hello");
+import dotenv from "dotenv";
+dotenv.config();
+import fastify from "fastify";
+import sensible from "@fastify/sensible";
+import { PrismaClient } from "@prisma/client";
+
+const app = fastify();
+app.register(sensible);
+const prisma = new PrismaClient();
+
+app.get("/posts", async (req, res) => {
+  return await commitToDb(prisma.post.findMany({
+    select: {
+      id: true,
+      title: true,
+    },
+  }));
+});
+
+async function commitToDb(promise) {
+    const [error, data] = await app.to(promise);
+    if (error) return app.httpErrors.internalServerError(error.message);
+    return data;
+}
+
+app.listen({ port: process.env.PORT }, () => {
+  console.log("Listening");
+});
