8548a1aee368a613d5cd96b68fa1ce9906fc4c9c
Parth Mangukiya <parthmangukiya@gofynd.com>
2023-07-31 01:44:24 +0530

    Rendering Root Comments

--- client-app/src/components/Comment.js
+++ client-app/src/components/Comment.js
@@ -1,3 +1,29 @@
-export function Comment({id, message}) {
-    return <h1>{message}</h1>
+import { FaEdit, FaHeart, FaReply, FaTrash } from "react-icons/fa";
+
+import { IconBtn } from "./IconBtn";
+
+const dateFormatter = new Intl.DateTimeFormat(undefined, {
+    dateStyle: "medium",
+    timeStyle: "short"
+});
+export function Comment({id, message, user, createdAt}) {
+    return (
+        <>
+            <div className="comment">
+                <div className="header">
+                    <span className="name">{user.name}</span>
+                    <span className="date">{dateFormatter.format(Date.parse(createdAt))}</span>
+                </div>
+                <div className="message">{message}</div>
+                <div className="footer">
+                    <IconBtn Icon={FaHeart} aria-label="Like"> 
+                        2
+                    </IconBtn>
+                    <IconBtn Icon={FaReply} aria-label="Reply" /> 
+                    <IconBtn Icon={FaEdit} aria-label="Edit" /> 
+                    <IconBtn Icon={FaTrash} aria-label="Trash" color="danger"/> 
+                </div>
+            </div>
+        </>
+    )
 }
