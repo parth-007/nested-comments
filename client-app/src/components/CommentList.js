85c6875da0da230b83e55354913e59df41dd6f25
Parth Mangukiya <parthmangukiya77@gmail.com>
2023-08-01 00:26:53 +0530

    Formatting

--- client-app/src/components/CommentList.js
+++ client-app/src/components/CommentList.js
@@ -1,10 +1,10 @@
 import { Comment } from "./Comment"
 
-export function CommentList({comments}){
+export function CommentList({ comments }) {
     return comments.map(comment => {
         return (
             <div key={comment.id} className="comment-stack">
-                <Comment {...comment}/>
+                <Comment {...comment} />
             </div>
         )
     })
